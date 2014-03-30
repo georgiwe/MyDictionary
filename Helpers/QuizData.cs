980c8b70bc6cf3df95ad212abc4e56505da085c7
Georgi Prodanov <georgiwe@gmail.com>
2014-03-30 11:39:41 +0300

    Add untracked files.

---
+++ Helpers/QuizData.cs
@@ -0,0 +1,42 @@
+﻿namespace MyDictionary.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MyDictionary.Helpers;
+    using MyDictionary.Enums;
+    using MyDictionary.Interfaces;
+
+    public class QuizData
+    {
+        private const string whenTrue = "Yes";
+        private const string whenFalse = "No";
+
+        private bool correctness;
+
+        public QuizData(int qNum, string question, string corrAnswer, string answer, bool correctness)
+        {
+            this.Question = question;
+            this.Answer = answer;
+            this.correctness = correctness;
+            this.Correct = corrAnswer;
+            this.QuestionNumber = qNum.ToString();
+        }
+
+        public string QuestionNumber { get; private set; }
+
+        public string Question { get; private set; }
+
+        public string Answer { get; private set; }
+
+        public string Correct { get; private set; }
+
+        public string Correctness
+        {
+            get
+            {
+                return this.correctness ? whenTrue : whenFalse;
+            }
+        }
+    }
+}
---
+++ Helpers/WordExistsException.cs
@@ -0,0 +1,25 @@
+﻿namespace MyDictionary.Helpers
+{
+    using MyDictionary.Interfaces;
+
+    public class WordExistsException : System.SystemException
+    {
+        public WordExistsException(string msg)
+            :base(msg)
+        {
+        }
+
+        public WordExistsException(string msg, IWord word)
+            :base(msg)
+        {
+            this.Word = word;
+        }
+
+        public WordExistsException(IWord word)
+        {
+            this.Word = word;
+        }
+
+        public IWord Word { get; private set; }
+    }
+}
